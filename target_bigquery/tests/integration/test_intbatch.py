53ff424df7269359b63f0d06a2d33604ece63573
Jonas Kalderstam <jonas@cowboyprogrammer.org>
2022-10-03 23:06:04 +0200

    Results from basic speed test
    Async: 15.710606 seconds
    Deflt: 31.171995 seconds
    
    And this is for 3 batch files containing 3 records each
    
    Not sure if several batch files will be common to receive

--- target_bigquery/tests/integration/test_intbatch.py
+++ target_bigquery/tests/integration/test_intbatch.py
@@ -1,21 +1,22 @@
 """Tests the Batch Sink"""
-# from unittest.mock import Mock
+from ...target import TargetBigQuery
+from .. import test_utils
+from .int_utils import get_client
+from datetime import datetime
 
-# from ...target import TargetBigQuery
-# from .. import test_utils
-# from .int_utils import get_client
 
+def test_loads_records_with_minimal_config():
+    """Non-batch aware tap with minimal config"""
+    target = TargetBigQuery(config = {}, parse_env_config=True)
 
-# def test_loads_records_with_minimal_config():
-#     """Non-batch aware tap with minimal config"""
-#     target = TargetBigQuery(config = {}, parse_env_config=True)
+    tap_lines = test_utils.get_test_tap_lines('batch_three.jsonl')
 
-#     tap_lines = test_utils.get_test_tap_lines('records_one_stream.jsonl')
+    start = datetime.now()
 
-#     target._process_lines(tap_lines)
-#     target._process_endofpipe()
+    target.listen(tap_lines)
 
-#     #query(tablename, column),
-#     #setofvalues[]
+    elapsed = datetime.now() - start
 
-#     assert(False)
+    print(f"Test took {elapsed}")
+
+    assert(False)
