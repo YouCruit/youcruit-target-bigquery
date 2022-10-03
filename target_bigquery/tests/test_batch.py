373843a7d9ab6a213b98439d8be3157665d34e43
Jonas Kalderstam <jonas@cowboyprogrammer.org>
2022-10-03 22:22:23 +0200

    Test for multiple batch files

--- target_bigquery/tests/test_batch.py
+++ target_bigquery/tests/test_batch.py
@@ -47,7 +47,7 @@ def test_loads_records_with_minimal_config(mock_client):
 
 
 @patch('target_bigquery.bq.Client', autospec=True)
-def test_loads_batch_with_minimal_config(mock_client):
+def test_loads_batch_one(mock_client):
     """Non-batch aware tap with minimal config"""
     target = TargetBigQuery(config = MINIMAL_CONFIG)
 
@@ -78,3 +78,37 @@ def test_loads_batch_with_minimal_config(mock_client):
     assert mock_client.return_value.query.call_args_list[0].contains('DROP TABLE `dataid`.`test_stream_')
     # Awaiting job
     mock_client.return_value.query.return_value.result.assert_called_once()
+
+
+@patch('target_bigquery.bq.Client', autospec=True)
+def test_loads_batch_three(mock_client):
+    """Non-batch aware tap with minimal config"""
+    target = TargetBigQuery(config = MINIMAL_CONFIG)
+
+    tap_lines = test_utils.get_test_tap_lines('batch_three.jsonl')
+
+    target.listen(file_input=tap_lines)
+    #target._process_lines(tap_lines)
+   # target._process_endofpipe()
+
+    mock_client.assert_called_once_with(project='projid', location=None)
+
+    # Temporary table is created with expiration
+    first_table_kwargs = mock_client.return_value.create_table.call_args_list[0].kwargs
+    assert first_table_kwargs['table'].expires is not None
+
+    # Data is loaded into table
+    mock_client.return_value.load_table_from_file.assert_called()
+    # Job is awaited
+    mock_client.return_value.load_table_from_file.return_value.result.assert_called()
+
+    # Real table is created without expiration
+    second_table_kwargs = mock_client.return_value.create_table.call_args_list[1].kwargs
+    assert second_table_kwargs['table'].expires is None
+
+    # Finish with merging data and dropping temp table
+    mock_client.return_value.query.assert_called()
+    assert mock_client.return_value.query.call_args_list[0].startswith('MERGE `dataid`.`test_stream`')
+    assert mock_client.return_value.query.call_args_list[0].contains('DROP TABLE `dataid`.`test_stream_')
+    # Awaiting job
+    mock_client.return_value.query.return_value.result.assert_called()
