df2cf81c6901d9f63f16ccbc39bf336415965cc2
Jonas Kalderstam <jonas@cowboyprogrammer.org>
2022-10-04 20:49:04 +0200

    This print is not very useful

--- target_bigquery/sinks.py
+++ target_bigquery/sinks.py
@@ -216,7 +216,6 @@ class BigQuerySink(BatchSink):
         storage: StorageTarget | None = None
 
         for path in files:
-            self.logger.info(f"[{self.stream_name}] Processing batch file {path}")
             head, tail = StorageTarget.split_url(path)
 
             if self.batch_config:
