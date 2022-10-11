87e825d25d54c8fa1f4b6872378b6a1addcaeb74
Jonas Kalderstam <jonas@cowboyprogrammer.org>
2022-10-11 11:07:59 +0200

    1 is probably fine

--- target_bigquery/target.py
+++ target_bigquery/target.py
@@ -18,7 +18,7 @@ class TargetBigQuery(Target):
     """Sample target for BigQuery."""
 
     # Override value in base class so we dont lose values that often
-    _MAX_RECORD_AGE_IN_MINUTES: float = 0.0
+    _MAX_RECORD_AGE_IN_MINUTES: float = 1.0
 
     name = "target-bigquery"
     config_jsonschema = th.PropertiesList(
